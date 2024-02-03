c26bdd380e935d30c31b0f2dac1f139063010a2f
Amirhossein donyaei <amirhossein.dn.wa@gmail.com>
2024-02-03 12:48:32 +0330

    initialize commit

---
+++ src/main/java/org/example/Clinet.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Clinet {
+    private static Socket socket;
+    private static BufferedReader in;
+    private static PrintWriter out;
+    private static DataInputStream dataIn;
+    private static ObjectInputStream objectIn;
+    private static ObjectOutputStream objectOut;
+    private static JSONObject json;
+    public static void main(String[] args) {
+        String IP = "127.0.0.1";
+        short port = 1234;
+        try {
+            socket = new Socket(IP, port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            objectIn = new ObjectInputStream(socket.getInputStream());
+            dataIn = new DataInputStream(socket.getInputStream());
+            objectOut = new ObjectOutputStream(socket.getOutputStream());
+
+            json = new JSONObject();
+            json.put("operation", "login");
+            json.put("username", "amir");
+            json.put("password", "1234");
+            json.put("device ID", "id34516545");
+            System.out.println(json);
+            objectOut.writeObject(json.toString());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Server is busy.");
+        }
+    }
+}
---
+++ src/main/java/org/example/ServerMain.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+    static short port = 1234;
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server started...");
+            DataBase.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            try {
+                System.out.println("Waiting for a new client ...");
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected: " + socket.getRemoteSocketAddress());
+                ClientHandler client = new ClientHandler(socket);
+                client.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
